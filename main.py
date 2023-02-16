51e53381f3345039d87b479eec2d481d6bd7cbd3
kalkidan12 <kalget12@gmail.com>
2023-02-16 08:32:08 +0300

    Split the dataset

--- main.py
+++ main.py
@@ -17,3 +17,6 @@ df.head()
 #DataFlair - Get the labels
 labels=df.label
 labels.head()
+
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
